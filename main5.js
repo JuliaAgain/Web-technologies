efdb36b19fc083f312804e355cc4266e0da36836
Julia <>
2023-09-15 12:14:10 +0300

    Added changes

---
+++ main5.js
@@ -0,0 +1,7 @@
+const paginationButtons = document.querySelectorAll('.pagination');
+for (let i = 0; i < paginationButtons; i++) {
+    const Buttons = paginationButtons[i];
+    Buttons.addEventListener('click', function(){
+        const page = button.getAttribute()
+    })
+}
